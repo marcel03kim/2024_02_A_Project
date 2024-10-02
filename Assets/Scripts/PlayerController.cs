a1a3ab8fa89793652e1171a60f9d0379f86580fb
hoyoons <belunar74@shingu.ac.kr>
2024-10-02 13:48:19 +0900

    feat : 3강까지

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    public float rotationSpeed = 10.0f;
+    
+    [Header("Camera settings")]
+    public Camera firstPersonCamera;
+    public Camera thirdPersonCamera;
+    public float mouseSensitivity = 2.0f;
+
+    public float radius = 5.0f;
+    public float minRadius = 1.0f;
+    public float maxRadius = 10.0f;
+
+    public float yMinLimit = -90f;
+    public float yMaxLimit = 90f;
+
+    private float theta = 0.0f;
+    private float phi = 0.0f;
+    private float targetVerticalRotation = 0;
+    private float verticalRotationSpeed = 240f;
+
+    private bool isFirstPerson = true;
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        SetupCameras();
+        SetActiveCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleJump();
+        HandleRotation();
+        HandleCameraToggle();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void SetupCameras()
+    {
+        firstPersonCamera.transform.localPosition = new Vector3 (0f, 0.6f, 0f);
+        firstPersonCamera.transform.localRotation = Quaternion.identity;
+    }
+
+    void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        theta += mouseX;
+        theta = Mathf.Repeat(theta, 360.0f);
+
+        targetVerticalRotation -= mouseY;
+        targetVerticalRotation = Mathf.Clamp(targetVerticalRotation, yMinLimit, yMaxLimit);
+        phi = Mathf.MoveTowards(phi, targetVerticalRotation, verticalRotationSpeed * Time.deltaTime);
+
+
+        if(isFirstPerson)
+        {
+            firstPersonCamera.transform.localRotation = Quaternion.Euler(phi, 0.0f, 0.0f);
+            transform.rotation = Quaternion.Euler(0.0f, theta, 0.0f);
+        }
+        else
+        {
+            float x = radius * Mathf.Sin(Mathf.Deg2Rad * phi) * Mathf.Cos(Mathf.Deg2Rad * theta);
+            float y = radius * Mathf.Cos(Mathf.Deg2Rad * phi);
+            float z = radius * Mathf.Sin(Mathf.Deg2Rad * phi) * Mathf.Sin(Mathf.Deg2Rad * theta);
+
+            thirdPersonCamera.transform.position = transform.position + new Vector3(x, y, z);
+            thirdPersonCamera.transform.LookAt(transform);
+
+            radius = Mathf.Clamp(radius - Input.GetAxis("Mouse ScrollWheel") * 5, minRadius, maxRadius);
+        }
+    }
+
+    void HandleCameraToggle()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            isFirstPerson = !isFirstPerson;
+            SetActiveCamera();
+        }
+    }
+
+    void SetActiveCamera()
+    {
+        firstPersonCamera.gameObject.SetActive(isFirstPerson);
+        thirdPersonCamera.gameObject.SetActive(!isFirstPerson);
+    }
+    void HandleMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement;
+
+        if(!isFirstPerson)
+        {
+            Vector3 cameraForward = thirdPersonCamera.transform.forward;
+            cameraForward.y = 0.0f;
+            cameraForward.Normalize();
+
+            Vector3 cameraRight = thirdPersonCamera.transform.right;
+            cameraRight.y = 0.0f;
+            cameraRight.Normalize();
+
+            movement = cameraRight * moveHorizontal + cameraForward * moveVertical; 
+            //Vector3 movement = cameraRight * moveHorizontal + cameraForward * moveVertical;
+            //rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            movement = transform.right * moveHorizontal + transform.forward * moveVertical; 
+            //Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
+            //rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        }
+
+        if(movement.magnitude > 0.1f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
